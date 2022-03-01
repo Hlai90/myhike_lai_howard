6b0fc93467ebb3d23f8202c717ee2659fb83ad25
Howard Lai <iihowaard@gmail.com>
2022-03-01 13:21:13 -0800

    added morning message and main js file

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBFteUMyoFr8R9iC9q54joVLA1DM4p0LPY",
+    authDomain: "fir-comp1800-ca6ac.firebaseapp.com",
+    projectId: "fir-comp1800-ca6ac",
+    storageBucket: "fir-comp1800-ca6ac.appspot.com",
+    messagingSenderId: "197343923410",
+    appId: "1:197343923410:web:f37be563150101ac4f710b"
 };
 
 //--------------------------------------------
